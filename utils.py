a5f8a94d482a554e461abdf8cbbf0e20e1260c3b
Paulina Kucharewicz <paulinakucharewicz2@gmail.com>
2024-05-20 20:43:11 +0200

    add update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
@@ -31,10 +33,45 @@ class CellDataset(Dataset):
 
         return img, None
 
+def hover_array(path):
+    """
+    Create numpy array suitable for hover input based on path to directory
+    containing image and masks
+    """
+    img_path = os.path.join(os.path.join(path, "images",os.listdir(os.path.join(path, "images"))[0]))
+    # pngs in a dataset contain alpha channel (always all ones) - we will use it for instance labels
+    # https://github.com/vqdang/hover_net/tree/master?tab=readme-ov-file#data-format
+    hov_input = plt.imread(img_path)
+    # setting 0 in 4th channel as background
+    masks_path = os.path.join(path, "masks")
+    hov_input[:,:,3] = 0
 
-def create_hover_data(path):
+    # for each mask, add mask's idx + 1 to the 4th channel 
+    # assumes that pxels with value 1 in masks do not overlap
+    for idx, mask_file in enumerate(os.listdir(masks_path)):
+        mask_array = plt.imread(os.path.join(masks_path, mask_file))
+        mask_array = mask_array * (idx + 1)
+        hov_input[:,:,3] += mask_array
+    
+    return hov_input
+
+    
+def create_hover_data(path, input_path = None):
     """
     For HoVerNet specific type of data is needed:
-    5 channels in npy file: first 3 RGB, instance label, instance type for each cell
+    4 channels in npy file: first 3 RGB and instance label for each cell
+
+    Parameters:
+        path: str
+            path to the train dataset
+        input_path: str
+            path the resulting .npy suitable for hover will be saved in
     """
-    pass
+    if not input_path:
+        input_path = os.path.join("hover_input", path)
+    if not os.path.exists(input_path):
+        os.mkdir(input_path)
+    for img_name in os.listdir(path):
+        example_path = os.path.join(path, img_name)
+        hov_array = hover_array(example_path)
+        np.save(os.path.join(input_path, f"{img_name}.npy"), hov_array)
