3b5f335d969307ad3d424ba876b32885c08219dd
stasiek <janikstasiek@gmail.com>
2024-05-27 17:53:39 +0200

    Added patching

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image
 import os
 
+from tqdm import tqdm
+
 
 class CellDataset(Dataset):
     def __init__(self, path, transform=None, train=True):
@@ -17,7 +19,7 @@ class CellDataset(Dataset):
         return len(self.imgs)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.path, self.imgs[idx], 'images', self.imgs[idx]+'.png')
+        img_path = os.path.join(self.path, self.imgs[idx], 'images', self.imgs[idx] + '.png')
         img = read_image(img_path)
         if self.transform is not None:
             img = self.transform(img)
@@ -27,36 +29,37 @@ class CellDataset(Dataset):
             mask = torch.zeros_like(img)
             for i, mask_path in enumerate(mask_paths):
                 mask_ = read_image(mask_path)
-                mask[mask_ != 0] = 1 * (i+1)
+                mask[mask_ != 0] = 1 * (i + 1)
 
             return img, mask
 
         return img, None
+
 
 def hover_array(path):
     """
     Create numpy array suitable for hover input based on path to directory
     containing image and masks
     """
-    img_path = os.path.join(os.path.join(path, "images",os.listdir(os.path.join(path, "images"))[0]))
+    img_path = os.path.join(os.path.join(path, "images", os.listdir(os.path.join(path, "images"))[0]))
     # pngs in a dataset contain alpha channel (always all ones) - we will use it for instance labels
     # https://github.com/vqdang/hover_net/tree/master?tab=readme-ov-file#data-format
     hov_input = plt.imread(img_path)
     # setting 0 in 4th channel as background
     masks_path = os.path.join(path, "masks")
-    hov_input[:,:,3] = 0
+    hov_input[:, :, 3] = 0
 
     # for each mask, add mask's idx + 1 to the 4th channel 
     # assumes that pxels with value 1 in masks do not overlap
     for idx, mask_file in enumerate(os.listdir(masks_path)):
         mask_array = plt.imread(os.path.join(masks_path, mask_file))
         mask_array = mask_array * (idx + 1)
-        hov_input[:,:,3] += mask_array
-    
+        hov_input[:, :, 3] += mask_array
+
     return hov_input
 
-    
-def create_hover_data(path, input_path = None):
+
+def create_hover_data(path, input_path=None):
     """
     For HoVerNet specific type of data is needed:
     4 channels in npy file: first 3 RGB and instance label for each cell
@@ -71,7 +74,30 @@ def create_hover_data(path, input_path = None):
         input_path = os.path.join("hover_input", path)
     if not os.path.exists(input_path):
         os.mkdir(input_path)
-    for img_name in os.listdir(path):
+    for img_name in tqdm(os.listdir(path)):
         example_path = os.path.join(path, img_name)
         hov_array = hover_array(example_path)
         np.save(os.path.join(input_path, f"{img_name}.npy"), hov_array)
+
+
+def crop_to_size(img, save_path):
+    shape = img.shape
+    crop = np.zeros((256, 256, 4))
+    for i in tqdm([k * 245 for k in range(shape[0] // 245)], desc=f"Saving to {save_path}", leave=False):
+        for j in [k * 245 for k in range(shape[1] // 245)]:
+            crop_ = img[i:i + 256, j:j + 256, ]
+            # plt.imshow(crop_)
+            # plt.show()
+            crop[:crop_.shape[0], :crop_.shape[1], ] = crop_
+            # plt.imshow(crop)
+            # plt.show()
+            print(crop_.shape, crop.shape)
+            np.save(save_path[:-4] + '(' + str(i) + ',' + str(j) + ').npy', crop)
+
+
+def crop_path(path, save_path):
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    for img_name in os.listdir(path):
+        img = np.load(os.path.join(path, img_name))
+        crop_to_size(img, os.path.join(save_path, img_name))
